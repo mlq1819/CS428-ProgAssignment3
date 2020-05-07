6911537a5a18d7cd24b74e33a5ed080faa02a8df
Michael Quinn <mquinn5@binghamton.edu>
2020-05-07 14:27:20 -0400

    beginning assignment

---
+++ program.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <time.h>
+#include <vector>
+
+#define DEBUG true
+
+using namespace std;
+
+vector<string> names;
+vector<vector<int>> table;
+
+
+
+int main() { 
+	
+}
